02f601209b3c318dbec1a59299abe521df1a277d
bachanasikh-codere <bachanasikh@gmail.com>
2025-11-25 21:41:41 +0400

    commit

---
+++ day 07/homework/homework.py
@@ -0,0 +1,52 @@
+#1)მოიყვანე and და or მაგალითი როგორც გაკვეთილზე გავაკეთეთ
+
+#2)ჩამოწერე and და or ყველა შესაძლო ვარიანტი
+
+#3)რომელი მონაცემთა ტიპი შეიცავს True-ს და False-ს?
+
+#4)რა შედეგს მივიღებთ ტერმინალში?(მაგალითები ქვემოთ არის)
+#True or False
+#false or false
+#true and true
+#true and False
+#flase or true
+
+#5)რას გამოიტანს ეკრანზე?
+ #print(15>3 or 15<3)
+#კომენტარის დახმარებით ახსენი დეტალურად თქვენი პასუხი
+
+#6)რას გამოიტასნ ეკრანზე ეს მაგალითი?
+#print(15>4 or 15 <9 and 12 > 3 or 0<19)
+
+
+#1)and და   or მაგალითი არის true and false
+
+#2)and და or -ის მაგალითია
+
+# AND
+#A	    B	    A AND B
+#True	True	True
+#True	False	False
+#False	True	False
+#False	False	False
+
+#OR
+#A	    B	    A OR B
+#True	True	True
+#True	False	True
+#False	True	True
+#False	False	False
+
+#3) bool შეიცავს
+
+#4)
+#True or False = True
+#False or False = False
+#True and True = True
+#True and False = False
+#False or True = True
+
+
+#5) ეკრანზე გამოიტანს true რადგან 15>3 მეტია და ეს სიმართლეა
+
+#6)print(15>4 or 15 <9 and 12 > 3 or 0<19) აქ გამოიტანს true
