cb8dbc6da5d799749f6f962cf84b0878db00cb6a
bachanasikh-codere <bachanasikh@gmail.com>
2026-06-15 23:11:48 +0400

    1

---
+++ day 02/homework/hw.py
@@ -0,0 +1,75 @@
+# 1) type() ფუნქცია აბრუნებს მონაცემის ტიპს.
+# მაგალითად: type("hello") დააბრუნებს <class 'str'>
+
+print(type("hello"))
+
+
+# 2) კონკატინაცია (Concatenation) ნიშნავს ტექსტების ერთმანეთთან შეერთებას.
+
+
+print("Hello" + " World")
+
+
+# 3) მონაცემთა ტიპები:
+# str  - ტექსტი
+# int  - მთელი რიცხვი
+# float - ათწილადი რიცხვი
+# bool - True ან False
+
+
+# 4) შექმენი str ტიპის ცვლადი და გამოიტანე ეკრანზე
+
+name = "Nika"
+print(name)
+
+
+# 5) შექმენი 2 int ტიპის ცვლადი, დაუმატე ერთმანეთს და გამოიტანე
+
+num1 = 10
+num2 = 20
+
+print(num1 + num2)
+
+
+# 6) ორ ცვლადზე შეასრულე სხვადასხვა მოქმედება
+
+a = 12
+b = 3
+
+print(a + b)   
+print(a - b)   
+print(a * b)   
+print(a / b)   
+print(a ** b) 
+print(a // b)  
+
+
+# 7) კონკატინაციის მაგალითი
+
+first_name = "Giorgi"
+last_name = "Beridze"
+
+print(first_name + " " + last_name)
+
+
+# 8) რა არის არასწორი?
+
+# name - "goa"
+# print(name
+
+# ეს:
+# 1. მინიჭებისთვის უნდა გამოვიყენოთ = და არა -
+# 2. print ფუნქციას აკლია დამხურავი ფრჩხილი )
+
+
+
+
+# 9) შექმენი 3 განსხვავებული ტიპის ცვლადი და შეამოწმე მათი ტიპი
+
+name = "Luka"     
+age = 15          
+height = 1.75      
+
+print(type(name))
+print(type(age))
+print(type(height))
---
+++ day 08/classwork/cw.py
@@ -0,0 +1,12 @@
+# 1)კომენტარებში ჩამოწერეთ ყველა control flows ტექნიკა
+# 1. if
+# 2. elif
+# 3. else
+# 4. for ციკლი
+# 5. while ციკლი
+# 6. break
+# 7. continue
+# 8. pass
+# 2)დაწერეთ კოდი for ციკლით, რომლითაც გამოიტანთ თქვენს სახელს 10-ჯერ
+for i in range(10):
+    print("Nika")  
